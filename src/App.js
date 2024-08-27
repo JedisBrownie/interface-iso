6c73017c6a4693a5234048d2c234c98df9902c56
Ismael-Raveloson <isma.raveloson@gmail.com>
2024-08-27 13:59:47 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import './assets/fomantic/dist/semantic.css';
+function App() {
+  return (
+    <div class="ui raised very padded text container segment">
+      <h2 class="ui header">Dogs Roles with Humans</h2>
+      <p></p>
+      <p></p>
+    </div>
+  );
+}
+
+export default App;

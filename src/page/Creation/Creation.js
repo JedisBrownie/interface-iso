40285c90a07cb453b2eae88f49ce272dbd0d272d
Ismael-Raveloson <isma.raveloson@gmail.com>
2024-09-10 15:43:14 +0300

    jodit

--- src/page/Creation/Creation.js
+++ src/page/Creation/Creation.js
@@ -1,31 +1,65 @@
 import "jodit";
-import { useState,useRef, useMemo } from "react";
+import { useState,useRef, useMemo,useEffect} from "react";
 import JoditEditor from "jodit-react";
 import HTMLReactParser from "html-react-parser/lib/index";
+import "./creation.css";
 
 export default function Creation({ placeholder }) {
 
     const editor = useRef(null);
     const[content,setContent] = useState('');
+    
+    const buttons = [
+        "undo",
+        "redo",
+        "bold",
+        "underline",
+        "italic",
+        "superscript",
+        "subscript",
+        "align",
+        "ul",
+        "ol",
+        "outdent",
+        "indent",
+        "font",
+        "fontsize",
+        "brush",
+        "paragraph",
+        "image",
+        "link",
+        "table",
+        "hr",
+        "eraser",
+        "copyformat",
+        "fullsize",
+        "selectall",
+        "print",
+      ];
 
-    const config = useMemo(() => {
+      const config = useMemo(() => {
         return {
-          readonly: false, // All options from https://xdsoft.net/jodit/docs/     
-          placeholder: placeholder || "Start typings...",
+          readonly: false,
+          placeholder: placeholder || "",
           uploader: {
             insertImageAsBase64URI: true,
           },
           showCharsCounter: false,
           showWordsCounter: false,
           showXPathInStatusbar: false,
+          askBeforePasteHTML: false,
+          askBeforePasteFromWord: false,
+          addNewLine: false,
+          buttons // No longer in dependency array
         };
       }, [placeholder]);
 
+ 
+
     return(
         <>
-            <JoditEditor ref={editor} value={content} onChange={newContent =>setContent(newContent)} config={config}>
-            </JoditEditor>
-            <div>{HTMLReactParser(content)}</div>
+          <JoditEditor ref={editor} value={content} onChange={newContent =>setContent(newContent)} config={config}>
+          </JoditEditor>
         </>
 
     );

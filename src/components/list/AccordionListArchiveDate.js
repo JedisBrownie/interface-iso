f989d486f15e83a5e2d8f0b4dfb011aa85a3c9b2
Ismael-Raveloson <120553853+Ismael-Raveloson@users.noreply.github.com>
2024-10-29 21:56:05 +0300

    update archive list

--- src/components/list/AccordionListArchiveDate.js
+++ src/components/list/AccordionListArchiveDate.js
@@ -13,157 +13,180 @@ import ListArchive from './ListArchive';
 
 export default class AccordionListArchiveDate extends Component{
     
-    state = {
-        activeYearIndex : 0,
-        activePgIndex : 0,
-        activePlIndex : 0,
-        allExpanded : false,
-    }
+    // state = {
+    //     activeYearIndex : 0,
+    //     activePgIndex : 0,
+    //     activePlIndex : 0,
+    //     allExpanded : false,
+    // }
 
-    handleClickYear = (e, titleProps) =>{
-        const { index } = titleProps
-        const { activeYearIndex , allExpanded } = this.state
+    // handleClickYear = (e, titleProps) =>{
+    //     const { index } = titleProps
+    //     const { activeYearIndex , allExpanded } = this.state
     
-        let newIndex;
-        if (allExpanded) {
-          newIndex = activeYearIndex === index ? -1 : index;
-        } else {
-          newIndex = index;
-        }
+    //     let newIndex;
+    //     if (allExpanded) {
+    //       newIndex = activeYearIndex === index ? -1 : index;
+    //     } else {
+    //       newIndex = index;
+    //     }
 
-        if(activeYearIndex === newIndex){
-            newIndex = '';
-        }
+    //     if(activeYearIndex === newIndex){
+    //         newIndex = '';
+    //     }
 
-        this.setState({ activeYearIndex: newIndex })
-    }
+    //     this.setState({ activeYearIndex: newIndex })
+    // }
 
-    handleClickPg = (e , titleProps) =>{
-        const { index } = titleProps
-        const { activePgIndex , allExpanded } = this.state
+    // handleClickPg = (e , titleProps) =>{
+    //     const { index } = titleProps
+    //     const { activePgIndex , allExpanded } = this.state
     
-        let newIndex;
-        if (allExpanded) {
-          newIndex = activePgIndex === index ? -1 : index;
-        } else {
-          newIndex = index;
-        }
+    //     let newIndex;
+    //     if (allExpanded) {
+    //       newIndex = activePgIndex === index ? -1 : index;
+    //     } else {
+    //       newIndex = index;
+    //     }
 
-        if(activePgIndex === newIndex){
-            newIndex = '';
-        }
+    //     if(activePgIndex === newIndex){
+    //         newIndex = '';
+    //     }
 
 
-        this.setState({ activePgIndex: newIndex })
-    }
+    //     this.setState({ activePgIndex: newIndex })
+    // }
 
-    handleClickPlIndex = (e , titleProps) =>{
-        const { index } = titleProps
-        const { activePlIndex , allExpanded } = this.state
+    // handleClickPlIndex = (e , titleProps) =>{
+    //     const { index } = titleProps
+    //     const { activePlIndex , allExpanded } = this.state
     
-        let newIndex;
-        if (allExpanded) {
-          newIndex = activePlIndex === index ? -1 : index;
-        } else {
-          newIndex = index;
-        }
+    //     let newIndex;
+    //     if (allExpanded) {
+    //       newIndex = activePlIndex === index ? -1 : index;
+    //     } else {
+    //       newIndex = index;
+    //     }
         
-        if(activePlIndex === newIndex){
-            newIndex = '';
-        }
+    //     if(activePlIndex === newIndex){
+    //         newIndex = '';
+    //     }
 
-        this.setState({ activePlIndex: newIndex })
-    }
+    //     this.setState({ activePlIndex: newIndex })
+    // }
 
-    renderLevel3 = (listeTypeDocument) =>{
+    state = {
+        activeIndexLevel1: null,
+        activeIndexLevel2: {},
+        activeIndexLevel3: {}
+      };
+    
+      handleClickLevel1 = (index) => {
+        this.setState((prevState) => ({
+          activeIndexLevel1: prevState.activeIndexLevel1 === index ? null : index,
+        }));
+      };
+    
+      handleClickLevel2 = (globalIndex, index) => {
+        this.setState((prevState) => ({
+          activeIndexLevel2: {
+            ...prevState.activeIndexLevel2,
+            [globalIndex]: prevState.activeIndexLevel2[globalIndex] === index ? null : index
+          }
+        }));
+      };
+    
+      handleClickLevel3 = (globalIndex, lieIndex, index) => {
+        this.setState((prevState) => ({
+          activeIndexLevel3: {
+            ...prevState.activeIndexLevel3,
+            [globalIndex]: {
+              ...prevState.activeIndexLevel3[globalIndex],
+              [lieIndex]: prevState.activeIndexLevel3[globalIndex]?.[lieIndex] === index ? null : index
+            }
+          }
+        }));
+    };
+
+
+    contentLevelTypeDocument = (listeTypeDocument) =>{
         console.log(listeTypeDocument);
         return(
             <ListArchive data={listeTypeDocument}></ListArchive>
         );
     }
 
-
-    // renderLevel2 = (listeProcessusLie) =>{
-    //     const panel2 = []; 
-
-    //     listeProcessusLie.forEach((processusLie) => {
-    //       panel2.push({
-    //         key: `processusLie-${processusLie.idProcessusLie}`,
-    //         title: processusLie.nomProcessusLie,
-    //         content: (
-    //             <AccordionAccordion>
-    //                 {this.renderLevel3(processusLie.listeTypeDocument)}
-    //             </AccordionAccordion>
-    //         ), 
-    //       });
-    //     });
-    //     console.log("panel 2 : " + panel2);
-
-    //     return panel2;
-    // }
-
-    contentLevel2 = () =>{
-        <div>
-            
-        </div>
-    }
-
-    renderLevel1 = (listeProcessusGlobal) =>{
-        const panel1 = [];
-
-        listeProcessusGlobal.forEach((processusGlobal) =>{
-            panel1.push({
-                key : `processusGlobal-${processusGlobal.idProcessusGlobal}`,
-                titre : processusGlobal.nomProcessusGlobal,
-                content : (
-                   <AccordionAccordion panels={this.renderLevel2(processusGlobal.listeProcessusLie)}></AccordionAccordion> 
-                )
-            })
-        });
+    contentLevelProcessusLie = (listeProcessusLie) =>{
         
-        console.log("panel 1 : " + panel1);
-        return panel1;
+        const panels = [];
+        var index = 0;
+
+        listeProcessusLie.forEach(processusLie => {
+            panels.push(
+                {
+                    key: `${processusLie.idProcessusLie}` , 
+                    title :  (
+                        <AccordionTitle active={this.state.activeIndexLevel2[processusLie.idProcessusLie] === index} onClick={() => this.handleClickLevel2(processusLie.idProcessusLie, index)}>
+                            <section className=''><Icon name='dropdown' />{processusLie.nomProcessusLie}</section>
+                        </AccordionTitle>
+                    ) , 
+                    content : (
+                        <AccordionContent active={this.state.activeIndexLevel2[processusLie.idProcessusLie] === index} >
+                            {this.contentLevelTypeDocument(processusLie.listeTypeDocument)}
+                        </AccordionContent>
+                    ) 
+                }
+            );
+            index = index + 1;
+        });
+
+        return(
+            <div>
+                <AccordionAccordion panels={panels}></AccordionAccordion>
+            </div>
+        );
     }
 
+
+
+
+    contentLevelProcessusGlobal = (listeProcessusGlobal) =>{
+        const panels = [];
+
+        listeProcessusGlobal.forEach(processusGlobal =>{
+            panels.push(
+                {
+                    key : `${processusGlobal.idProcessusGlobal}` , 
+                    title : `${processusGlobal.nomProcessusGlobal}` , 
+                    content : this.contentLevelProcessusLie(processusGlobal.listeProcessusLie) 
+                }
+            )
+        });
+
+        return(
+            <div>
+                <AccordionAccordion panels={panels}></AccordionAccordion>
+            </div>
+        );
+    }
     
+    contentLevelAll = (dataArchiveProcessus) =>{
+        const panels = [];
 
+        dataArchiveProcessus.forEach(archive =>{
+            panels.push({key:`${archive.date}` , title : `${archive.date}` , content : this.contentLevelProcessusGlobal(archive.listeProcessusGlobal)})
+        });
 
-    // renderAllLevel = (dataArchiveParDate) =>{
-    //     const panel = dataArchiveParDate.map((archive) => (
-    //         {
-    //             key : `annee-${archive.date}`,
-    //             titre : archive.date,
-    //             content : (
-    //                 <Accordion panels={archive.listeProcessusGlobal} defaultActiveIndex={0}></Accordion>
-    //             )
-    //         }
-    //     ));
-    //     return panel ;
-    // }
-
-    // renderAllLevel = (dataArchiveParDate) =>{
-    //     const panel = [];
-
-    //     dataArchiveParDate.forEach((archive) =>{
-    //         panel.push({
-    //             key : `annee-${archive.date}`,
-    //             titre : archive.date,
-    //             content : (
-    //                 <Accordion panels={archive.listeProcessusGlobal} defaultActiveIndex={0}></Accordion>
-    //             )
-    //         })
-    //     });
-
-    //     console.log("panel : " + panel);
-    //     return panel;
-    // }
-
+        return(
+            <Accordion defaultActiveIndex={0} panels={panels}></Accordion>
+        )
+    }
 
 
 
 
     render(){
-        const {activeYearIndex,activePgIndex,activePlIndex} = this.state;
+        // const {activeYearIndex,activePgIndex,activePlIndex} = this.state;
         const {data} = this.props;
 
         
@@ -204,6 +227,62 @@ export default class AccordionListArchiveDate extends Component{
                 ]},
             ]}
         ];
+
+        const listeProcessusGlobal = [
+            {idProcessusGlobal : '1000' , nomProcessusGlobal : 'Processus Management',listeProcessusLie : [
+                {idProcessusLie : '1100' , nomProcessusLie : 'Planification' , listeTypeDocument : [
+                    
+                    {idTypeDocument : 4 , nomTypeDocument : 'Enregistrement' , listeDocument : [
+                        {referenceDoc : 'EN1100-20001205',idDocument:1,nom:'Indicateurs et Objectifs 2008',status:'Archive',nombreRevision:8,dateArchive:'31/03/2004'},
+                        {referenceDoc : 'EN1100-20001205',idDocument:2,nom:'Indicateurs et Objectifs 2008',status:'Archive',nombreRevision:8,dateArchive:'31/03/2004'},
+                    ]},
+                    {idTypeDocument : 3 , nomTypeDocument : 'Fiche d\'instruction' , listeDocument : [
+                        {referenceDoc : 'FI1100-20001205',idDocument:1,nom:'TT - Déchargement des ajouts en big bag',status:'Archive',nombreRevision:2,dateArchive:'31/03/2004'},
+                        {referenceDoc : 'FI1100-20001205',idDocument:2,nom:'TT - Déchargement des ajouts en big bag',status:'Archive',nombreRevision:2,dateArchive:'31/03/2004'},
+                    ]},
+                ]},
+
+                {idProcessusLie : '1200' , nomProcessusLie : 'Revue de direction' , listeTypeDocument : [
+                    
+                    {idTypeDocument : 4 , nomTypeDocument : 'Enregistrement' , listeDocument : [
+                        {referenceDoc : 'EN1100-20001205',idDocument:1,nom:'Indicateurs et Objectifs 2008',status:'Archive',nombreRevision:8,dateArchive:'31/03/2004'},
+                        {referenceDoc : 'EN1100-20001205',idDocument:2,nom:'Indicateurs et Objectifs 2008',status:'Archive',nombreRevision:8,dateArchive:'31/03/2004'},
+                    ]},
+                    {idTypeDocument : 3 , nomTypeDocument : 'Fiche d\'instruction' , listeDocument : [
+                        {referenceDoc : 'FI1100-20001205',idDocument:1,nom:'TT - Déchargement des ajouts en big bag',status:'Archive',nombreRevision:2,dateArchive:'31/03/2004'},
+                        {referenceDoc : 'FI1100-20001205',idDocument:2,nom:'TT - Déchargement des ajouts en big bag',status:'Archive',nombreRevision:2,dateArchive:'31/03/2004'},
+                    ]},
+                ]}
+
+            ]} ,
+            
+            {idProcessusGlobal : '2000' , nomProcessusGlobal : 'Ressources Mandeha',listeProcessusLie : [
+                {idProcessusLie : '2100' , nomProcessusLie : 'Ressources Humaines' , listeTypeDocument : [
+                    
+                    {idTypeDocument : 4 , nomTypeDocument : 'Enregistrement' , listeDocument : [
+                        {referenceDoc : 'EN1100-20001205',idDocument:1,nom:'Indicateurs et Objectifs 2008',status:'Archive',nombreRevision:8,dateArchive:'31/03/2004'},
+                        {referenceDoc : 'EN1100-20001205',idDocument:2,nom:'Indicateurs et Objectifs 2008',status:'Archive',nombreRevision:8,dateArchive:'31/03/2004'},
+                    ]},
+                    {idTypeDocument : 3 , nomTypeDocument : 'Fiche d\'instruction' , listeDocument : [
+                        {referenceDoc : 'FI1100-20001205',idDocument:1,nom:'TT - Déchargement des ajouts en big bag',status:'Archive',nombreRevision:2,dateArchive:'31/03/2004'},
+                        {referenceDoc : 'FI1100-20001205',idDocument:2,nom:'TT - Déchargement des ajouts en big bag',status:'Archive',nombreRevision:2,dateArchive:'31/03/2004'},
+                    ]},
+                ]},
+
+                {idProcessusLie : '2300' , nomProcessusLie : 'Travaux neufs' , listeTypeDocument : [
+                    
+                    {idTypeDocument : 4 , nomTypeDocument : 'Enregistrement' , listeDocument : [
+                        {referenceDoc : 'EN1100-20001205',idDocument:1,nom:'Indicateurs et Objectifs 2008',status:'Archive',nombreRevision:8,dateArchive:'31/03/2004'},
+                        {referenceDoc : 'EN1100-20001205',idDocument:2,nom:'Indicateurs et Objectifs 2008',status:'Archive',nombreRevision:8,dateArchive:'31/03/2004'},
+                    ]},
+                    {idTypeDocument : 3 , nomTypeDocument : 'Fiche d\'instruction' , listeDocument : [
+                        {referenceDoc : 'FI1100-20001205',idDocument:1,nom:'TT - Déchargement des ajouts en big bag',status:'Archive',nombreRevision:2,dateArchive:'31/03/2004'},
+                        {referenceDoc : 'FI1100-20001205',idDocument:2,nom:'TT - Déchargement des ajouts en big bag',status:'Archive',nombreRevision:2,dateArchive:'31/03/2004'},
+                    ]},
+                ]}
+
+            ]},
+        ]
 
         // return(
         //     <>
@@ -257,9 +336,9 @@ export default class AccordionListArchiveDate extends Component{
 
         return(
             <>
-                {this.renderLevel3(listeTypeDocument)}
+                {this.contentLevelAll(data)}
             </>
-        )
+        );
     }
 
 }
